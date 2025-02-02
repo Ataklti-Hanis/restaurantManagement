6ecabac61a3b94447c345f1f88478419ed7af6dd
AtakltiHanis <atakltihanis14@gmail.com>
2025-02-03 00:49:32 +0300

    json add

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -2,15 +2,41 @@
   <img class="logo" src="../assets/restourant.png" alt="" />
   <h1>Sign Up</h1>
   <div class="register">
-    <input type="text" placeholder="Enter you name" />
-    <input type="text" placeholder="Enter you email" />
-    <input type="password" placeholder="Enter you password" />
-    <button>Register</button>
+    <input type="text" v-model="name" placeholder="Enter you name" />
+    <input type="text" v-model="email" placeholder="Enter you email" />
+    <input
+      type="password"
+      v-model="password"
+      placeholder="Enter you password"
+    />
+    <button v-on:click="SignUp">Register</button>
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
   name: "SignUp",
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async SignUp() {
+      let result = await axios.post("http://localhost:3000/users", {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+      });
+
+      console.warn(result);
+      if (result.status == 201) {
+        alert("Signed up sucessfully");
+      }
+    },
+  },
 };
 </script>
 
