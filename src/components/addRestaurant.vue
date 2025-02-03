c22a2d04e02770c4767192ba57f4d73754cd4163
AtakltiHanis <atakltihanis14@gmail.com>
2025-02-03 10:23:54 +0300

    Form add

--- src/components/addRestaurant.vue
+++ src/components/addRestaurant.vue
@@ -1,6 +1,20 @@
 <template>
   <HeaderPage />
   <h1>Hello user, welcome on Add Restaurant page</h1>
+  <form class="add" action="">
+    <input type="text" placeholder="Enter name" v-model="restaurant.name" />
+    <input
+      type="text"
+      placeholder="Enter address"
+      v-model="restaurant.address"
+    />
+    <input
+      type="text"
+      placeholder="Enter contact"
+      v-model="restaurant.contact"
+    />
+    <button type="button" @click="addRestaurant">Add new Restaurant</button>
+  </form>
 </template>
 <script>
 import HeaderPage from "./HeaderPage.vue";
@@ -8,6 +22,20 @@ export default {
   name: "addRestaurant",
   components: {
     HeaderPage,
+  },
+  data() {
+    return {
+      restaurant: {
+        name: "",
+        address: "",
+        contact: "",
+      },
+    };
+  },
+  methods: {
+    addRestaurant() {
+      console.warn(this.restaurant);
+    },
   },
   mounted() {
     let user = localStorage.getItem("user-info");
